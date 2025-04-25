f1a6ccb46348cdd2535b21890b97fccc5214daca
Rafael <rafaelbthomaz@gmail.com>
2025-04-25 13:32:15 -0300

    adding letters used component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from "./components/Header"
 import { Letter } from "./components/Letter"
 import { Input } from "./components/Input"
 import { Button } from "./components/Button"
+import { LettersUsed } from "./components/LettersUsed"
 import { Tip } from "./components/Tip"
 
 
@@ -34,6 +35,8 @@ export default function App() {
           <Input autoFocus maxLength={1} placeholder="?"/>
           <Button title="Confirmar" />
         </div>
+
+        <LettersUsed />
       </main>
     </div>
   )
---
+++ src/components/LettersUsed/index.tsx
@@ -0,0 +1,17 @@
+import styles from "./style.module.css"
+
+import { Letter } from "../Letter"
+
+export function LettersUsed() {
+  return (
+    <div className={styles.lettersUsed}>
+     <h5>Letras Utilizadas</h5>
+
+     <div>
+      <Letter value="R" size="small" color="correct" />
+      <Letter value="X" size="small" color="wrong" />
+     </div>
+    </div>
+
+  )
+}
