af32eb899816aaf0fc4c4256658d9f1a88fc88c6
Rafael <rafaelbthomaz@gmail.com>
2025-04-25 12:06:14 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import styles from "./app.module.css"
+
+import { Header } from "./components/Header"
+
+
+export default function App() {
+
+  function handleRestartGame() {
+    alert("Reinicar o jogo?")
+  }
+
+  return (
+    <div className={styles.container}>
+      <main>
+        <Header current={5} max={10} onRestart={handleRestartGame} />
+      </main>
+    </div>
+  )
+}
